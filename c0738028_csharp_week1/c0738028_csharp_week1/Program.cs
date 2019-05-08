7beda2970ed2e80c94891c812ce4ee5a5a1d6eb5
Amandeep Kaur <738028@student.tor.loc>
2019-05-08 11:08:33 -0400

    code done

--- c0738028_csharp_week1/c0738028_csharp_week1/Program.cs
+++ c0738028_csharp_week1/c0738028_csharp_week1/Program.cs
@@ -10,13 +10,15 @@ namespace c0738028_csharp_week1
             int start = 314;
             int end = 102;
             Console.WriteLine("Sum of the numbers");
-            for (int i= start; i< end; i--)
+            for (int i= start; i> end; i-=6)//going down by 6
             {
 
-               sum += start;
-               Console.WriteLine(start);
+               sum += i;
+                //Console.WriteLine(" Now Number is" + i);
+                //Console.WriteLine("Now sum is" +sum);
             }
-        
+
+            Console.WriteLine("The answer is " +sum);
             Console.ReadLine();
         }
       
