15869564962d1b5a7f5ab1b570c0e0a75825a86d
Amandeep Kaur <738028@student.tor.loc>
2019-05-08 10:41:07 -0400

    started vs project

---
+++ c0738028_csharp_week1/c0738028_csharp_week1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace c0738028_csharp_week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int start = 314;
+            int end = 102;
+            for (int i= start;)
+        }
+    }
+}
